af81aa31f95f040dc62dc6cf41433c9b0a160bb1
mwoolf87 <michael.j.woolf@gmail.com>
2021-10-02 15:16:21 -0400

    registration path creation

--- routes/user.js
+++ routes/user.js
@@ -1,65 +1,64 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
+const bcrypt = require("bcrypt");
+const Sequelize = require("sequelize");
+const { User } = require("../models");
 
-const Sequelize = require('sequelize');
-const { User } = require('../models');
-
-router.get('/', async (req, res) => {
-    res.setHeader("Content-Type", "application/json");
-    const user = await User.findAll();
-    res.json(user);
+router.get("/", async (req, res) => {
+  res.setHeader("Content-Type", "application/json");
+  const user = await User.findAll();
+  res.json(user);
 });
 
-router.get('/:id', async (req, res) => {
-    try {
-        const oneUser = await User.findByPk(req.params.id);
-        res.json(oneUser);
-    } catch (e) {
-        console.log(e);
-        res.status(404).json({
-            message: 'User not found'
-        });
+router.get("/:id", async (req, res) => {
+  try {
+    const oneUser = await User.findByPk(req.params.id);
+    res.json(oneUser);
+  } catch (e) {
+    console.log(e);
+    res.status(404).json({
+      message: "User not found"
+    });
+  }
+});
+
+router.post("/", async (req, res) => {
+  // req.body contains an Object with username, email, password
+  const { username, email, password } = req.body;
+  const newUser = await User.create({
+    username,
+    email,
+    password
+  });
+
+  // Send back the new user's ID in the response:
+  res.json({
+    id: newUser.id
+  });
+});
+
+router.put("/:id", async (req, res) => {
+  const { id } = req.params;
+
+  // Assuming that `req.body` is limited to
+  // the keys firstName, lastName, and email
+  const updatedUser = await User.update(req.body, {
+    where: {
+      id
     }
+  });
+
+  res.json(updatedUser);
 });
 
-router.post('/', async (req, res) => {
-    // req.body contains an Object with username, email, password
-    const { username, email, password, } = req.body;
-    const newUser = await User.create({
-        username,
-        email,
-        password,
-    });
-
-    // Send back the new user's ID in the response:
-    res.json({
-        id: newUser.id
-    });
+router.delete("/:id", async (req, res) => {
+  const { id } = req.params;
+  const deletedUser = await User.destroy({
+    where: {
+      id
+    }
+  });
+  res.json(deletedUser);
 });
-
-router.put('/:id', async (req, res) => {
-    const { id } = req.params;
-
-    // Assuming that `req.body` is limited to
-    // the keys firstName, lastName, and email
-    const updatedUser = await User.update(req.body, {
-        where: {
-            id
-        }
-    });
-
-    res.json(updatedUser);
-});
-
-router.delete('/:id', async (req, res) => {
-    const { id } = req.params;
-    const deletedUser = await User.destroy({
-        where: {
-            id
-        }
-    });
-    res.json(deletedUser);
-});
-
 
 module.exports = router;
