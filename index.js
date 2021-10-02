6894fef0829bcd96d1067931e765c5b9bb12a588
Teyon Price <teyon81@gmail.com>
2021-10-02 13:03:56 -0400

    test

--- index.js
+++ index.js
@@ -6,9 +6,26 @@ const port = 3000;
 const express = require('express');
 const app = express();
 
+<<<<<<< Updated upstream
 require("./startup/routes")(app);
 
 
+=======
+// const es6Renderer = require("express-es6-template-engine");
+// app.engine("html", es6Renderer);
+// app.set("views", "templates");
+// app.set("view engine", "html");
+
+const server = http.createServer(app);
+// const db = require("./db");
+
+app.get('/user', async (req, res) => {
+    res.setHeader("Content-Type", "application/json");
+    const user = await User.findAll();
+    res.json(user);
+})
+
+>>>>>>> Stashed changes
 app.listen(port, hostname, () => {
     console.log(`Server running at http://${hostname}:${port}/`);
 });
