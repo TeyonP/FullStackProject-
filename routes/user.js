cb1ab9f207a499e2c17e14ec112bef5e5d3a7639
Gabriel Lopez <lopezg3000@gmail.com>
2021-10-02 10:31:15 -0500

    crud operations added to user js

--- routes/user.js
+++ routes/user.js
@@ -10,4 +10,58 @@ router.get('/', async (req, res) => {
     res.json(user);
 });
 
+router.get('/:id', async (req, res) => {
+    try {
+        const oneUser = await User.findByPk(req.params.id);
+        res.json(oneUser);
+    } catch (e) {
+        console.log(e);
+        res.status(404).json({
+            message: 'User not found'
+        });
+    }
+});
+
+router.post('/', async (req, res) => {
+    // req.body contains an Object with username, email, password
+    const { username, email, password, } = req.body;
+    const newUser = await User.create({
+        username,
+        email,
+        password,
+    });
+
+    // Send back the new user's ID in the response:
+    res.json({
+        id: newUser.id
+    });
+});
+
+router.put('/:id', async (req, res) => {
+    const { id } = req.params;
+
+    // Assuming that `req.body` is limited to
+    // the keys firstName, lastName, and email
+    const updatedUser = await User.update(req.body, {
+        where: {
+            id
+        }
+    });
+
+    res.json(updatedUser);
+});
+
+router.delete('/:id', async (req, res) => {
+    const { id } = req.params;
+    const deletedUser = await User.destroy({
+        where: {
+            id
+        }
+    });
+    res.json(deletedUser);
+});
+
+
+
+
 module.exports = router;
