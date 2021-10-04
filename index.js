87469675e3b0ebc25bd31af41442ff97fa249ed1
mwoolf87 <michael.j.woolf@gmail.com>
2021-10-04 00:00:22 -0400

    created a new branch, applied some changes based off the most recent pulled down main, added in my previous changes, installed cors via npm/added cors in index.js

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
-const http = require('http');
+const http = require("http");
 
-const hostname = '127.0.0.1';
+const hostname = "127.0.0.1";
 const port = 3000;
 
-const express = require('express');
+const express = require("express");
 const app = express();
+const cors = require("cors");
+app.use(cors());
+app.use(express.json());
 
 require("./startup/routes")(app);
 
-
 app.listen(port, hostname, () => {
-    console.log(`Server running at http://${hostname}:${port}/`);
+  console.log(`Server running at http://${hostname}:${port}/`);
 });
