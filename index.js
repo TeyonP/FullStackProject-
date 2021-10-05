b74a98049806a8c5e504dfaf6311e09fc5f0cf4e
Teyon Price <teyon81@gmail.com>
2021-10-05 19:04:31 -0400

    index file

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const http = require("http");
 
 const hostname = "127.0.0.1";
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 const express = require("express");
 const app = express();
