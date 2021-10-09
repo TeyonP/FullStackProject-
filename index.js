51e2e5827173a83a0c751a53b4e79cea87ecc135
Gabriel Lopez <lopezg3000@gmail.com>
2021-10-09 10:09:32 -0500

    deploy bug took out some require files

--- index.js
+++ index.js
@@ -2,17 +2,17 @@ const http = require("http");
 
 const hostname = "127.0.0.1";
 const port = process.env.PORT || 3000;
-const db = require("./models");
+// const db = require("./models");
 const express = require("express");
 const app = express();
-const cors = require("cors");
-const path = require("path");
+// const cors = require("cors");
+// const path = require("path");
 app.use(cors());
 app.use(express.json());
-app.use(express.static(__dirname + '/public'));
+// app.use(express.static(__dirname + '/public'));
 
-const Sequelize = require("sequelize");
-const { anchorChart } = require("./models");
+// const Sequelize = require("sequelize");
+// const { anchorChart } = require("./models");
 
 const es6Renderer = require("express-es6-template-engine");
 app.engine("html", es6Renderer);
@@ -30,20 +30,20 @@ app.get("/", async (req, res) => {
     // });
 });
 
-app.get("/shoppingpage", (req, res) => {
-    // console.log("route is working");
-    // res.json({});
-    db.anchorChart.findAll().then(results => {
-        console.log(results);
-        // res.json({});
-        res.render("shopping_page", {
-            locals: {
-                anchorCharts: results
-            }
-        });
-    });
-});
-require("./startup/routes")(app);
+// app.get("/shoppingpage", (req, res) => {
+//     // console.log("route is working");
+//     // res.json({});
+//     db.anchorChart.findAll().then(results => {
+//         console.log(results);
+//         // res.json({});
+//         res.render("shopping_page", {
+//             locals: {
+//                 anchorCharts: results
+//             }
+//         });
+//     });
+// });
+// require("./startup/routes")(app);
 
 app.listen(port, () => {
     console.log(`Server running `);
