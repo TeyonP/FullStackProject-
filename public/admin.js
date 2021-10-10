f8d061faa252c26858bfe0121363fb70bf5f216c
mwoolf87 <michael.j.woolf@gmail.com>
2021-10-10 16:13:34 -0400

    start of put crud

--- public/admin.js
+++ public/admin.js
@@ -27,3 +27,22 @@ btnAddAnchorChart.addEventListener("click", e => {
 
   location.replace("http://localhost:3000/admin");
 });
+
+updateBtn.addEventListener("click", e => {
+  e.preventDefault();
+  console.log("clicked");
+  const currentAnchorChart = {
+    topic: inputTopic.value,
+    subject: inputSubject.value,
+    grade: inputGrade.value,
+    price: inputPrice.value
+  };
+  console.log(currentAnchorChart);
+  fetch("http://localhost:3000/anchorChart/:id", {
+    method: "Get",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(currentAnchorChart)
+  });
+});
